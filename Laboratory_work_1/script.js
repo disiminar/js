dba819e6f4d5d1a34c0fa34deb286e953de84c4b
new dawn fades <91181816+disiminar@users.noreply.github.com>
2024-02-24 16:03:49 +0200

    basic functionality

---
+++ Laboratory_work_1/script.js
@@ -0,0 +1,22 @@
+function triangle(val1 = 3, type1 = "leg", val2 = 4, type2 = "leg") {
+    const validTypes = [
+      "leg",
+      "hypotenuse",
+      "adjacent angle",
+      "opposite angle",
+      "angle",
+    ];
+
+    let a, b, c, alpha, beta;
+
+    console.log(
+        'triangle (7, "leg", "8", "hypotenuse")', "\n",
+        "a:", a, "\n",
+        "b:", b, "\n",
+        "c:", c, "\n",
+        "alpha:", alpha, "\n",
+        "beta:", beta );
+      return "success";
+    }
+
+    triangle (7, "leg", "8", "hypotenuse")
