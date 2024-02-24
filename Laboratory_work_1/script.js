69b21bb4eb0dd845c7958c16a7e429808cef593a
new dawn fades <91181816+disiminar@users.noreply.github.com>
2024-02-24 16:34:08 +0200

    check, validation & calculation done

--- Laboratory_work_1/script.js
+++ Laboratory_work_1/script.js
@@ -9,6 +9,8 @@ function triangle(val1 = 3, type1 = "leg", val2 = 4, type2 = "leg") {
 
     const isValidType = (type) => validTypes.includes(type);
     const isValidValue = (val) => val > 0 && typeof val === "number";
+    const toDegrees = (angle) => angle * (180 / Math.PI);
+    const toRadians = (angle) => angle * (Math.PI / 180);
 
     let a, b, c, alpha, beta;
 
@@ -32,49 +34,70 @@ function triangle(val1 = 3, type1 = "leg", val2 = 4, type2 = "leg") {
 
     switch (type1) {
         case "leg":
-          //
-          switch (type2) {
-            case "hypotenuse":
-                //
-                break;
-            case "leg":
-                // 
-                break;
-            case "adjacent angle":
-                //
-                break;
-            case "opposite angle":
-                //
-                break;
-            default:
-                console.log("Minor spelling mistake o_0");
-                return "failed";        
+            a = val1;
+            switch (type2) {
+                case "hypotenuse":
+                    c = val2;
+                    b = Math.sqrt(c * c - a * a);
+                    alpha = toDegrees(Math.asin(a / c));
+                    beta = 90 - alpha;
+                    break;
+                case "leg":
+                    b = val2;
+                    c = Math.sqrt(a * a + b * b);
+                    alpha = toDegrees(Math.asin(a / c));
+                    beta = 90 - alpha;
+                    break;
+                case "adjacent angle":
+                    beta = val2;
+                    alpha = 90 - beta;
+                    c = a / Math.cos(toRadians(beta));
+                    b = Math.sqrt(c * c - a * a);
+                    break;
+                case "opposite angle":
+                    alpha = val2;
+                    beta = 90 - alpha;
+                    c = a / Math.sin(toRadians(alpha));
+                    b = Math.sqrt(c * c - a * a);
+                    break;
+                default:
+                    console.log("Minor spelling mistake o_0");
+                    return "failed";        
           }
-        //
         break;
         case "hypotenuse":
-            //
+            c = val1;
             switch (type2) {
                 case "leg":
-                    //
+                    a = val2;
+                    b = Math.sqrt(c * c - a * a);
+                    alpha = toDegrees(Math.asin(a / c));
+                    beta = 90 - alpha;
                     break;
                 case "angle":
-                    //
+                    alpha = val2;
+                    beta = 90 - alpha;
+                    a = c * Math.sin(toRadians(alpha));
+                    b = Math.sqrt(c * c - a * a);
                     break;
-            default:
-            console.log("Minor spelling mistake o_0");
-            return "failed";
+                default:
+                    console.log("Minor spelling mistake o_0");
+                    return "failed";
             } 
-        //
         break;
         case "adjacent angle":
-            //
+            alpha = val1;
+            beta = 90 - alpha;
             switch (type2) {
                 case "leg":
-                    //
+                    a = val2;
+                    c = a / Math.cos(toRadians(alpha));
+                    b = Math.sqrt(c * c - a * a);
                     break;
                 case "hypotenuse":
-                    //
+                    c = val2;
+                    a = c * Math.sin(toRadians(alpha));
+                    b = Math.sqrt(c * c - a * a);
                     break;
                 default:
                     console.log("Minor spelling mistake o_0");
@@ -82,13 +105,18 @@ function triangle(val1 = 3, type1 = "leg", val2 = 4, type2 = "leg") {
             }
         break;
         case "opposite angle":
-            //
+            alpha = val1;
+            beta = 90 - alpha;
             switch (type2) {
                 case "leg":
-                    //
+                    a = val2;
+                    c = a / Math.sin(toRadians(alpha));
+                    b = Math.sqrt(c * c - a * a);
                     break;
                 case "hypotenuse":
-                    //
+                    c = val2;
+                    a = c * Math.cos(toRadians(alpha));
+                    b = Math.sqrt(c * c - a * a);
                     break;
                 default:
                     console.log("Minor spelling mistake o_0");
@@ -96,7 +124,11 @@ function triangle(val1 = 3, type1 = "leg", val2 = 4, type2 = "leg") {
             }
             break;
         case "angle":
-            //
+            alpha = val1;
+            beta = 90 - alpha;
+            c = val2;
+            a = c * Math.sin(toRadians(alpha));
+            b = Math.sqrt(c * c - a * a);
             break;
         }
 
