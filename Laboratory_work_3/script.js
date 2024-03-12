635e9c338898ad2c49bd1cefa7581d23bdd82c98
new dawn fades <91181816+disiminar@users.noreply.github.com>
2024-03-12 23:05:33 +0200

    car1 initialization & values

--- Laboratory_work_3/script.js
+++ Laboratory_work_3/script.js
@@ -1,1 +1,10 @@
-123
+let car1 = new Object();
+car1.color = white;
+car1.maxSpeed = 197; // 197mph O.o
+car1.tuning = true;
+car1["number of accidents"] = 0;
+
+car1.driver = new Object();
+car1.driver.name = "Oleh Zapukhliak";
+car1.driver.category = "C";
+car1.driver["personal limitations"] = "No driving at night";
